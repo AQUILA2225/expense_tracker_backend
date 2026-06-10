c37ebd6b6ea0257feadb7a2a2ce5b19905668a6b
Aquila <valluribharathi01@gmail.com>
2026-06-10 11:46:41 +0530

    removed cors code errors

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"], # Allow all frontends
     allow_credentials=True,
-    allow_methods=["*"] # GET, POST, PUT, DELETE
+    allow_methods=["*"], # GET, POST, PUT, DELETE
     allow_headers=["*"]
 )
 
